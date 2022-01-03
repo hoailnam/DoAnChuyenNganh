cb2e8d559e834d47bb18d2ecd09bce28d7bbda68
hoailnam <dh51805129@student.stu.edu.vn>
2022-01-03 21:29:23 +0700

    reset

--- admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['isLogin'])) {
-    header("Location: ./view/login.php");
-}else{
-    header("Location: ./view/index.php");
-}
--- admin/view/layout/Footer.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-      <div class="copyright">
-        <p>Copyright &copy; Designed & Developed by <a>Quốc Đạt</a> 2021</p>
-      </div>
-    </div>
