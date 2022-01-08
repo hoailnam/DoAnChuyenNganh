4bd03ec871744d637e60f8d400fec0d88ed3a990
hoailnam <dh51805129@student.stu.edu.vn>
2022-01-08 20:27:40 +0700

    a

--- admin/view/logoutpage.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-$_SESSION['isLoginAdmin']= false;
-$_SESSION["admin_email"] ="";
-echo '<script language="javascript">';
-echo 'alert("Đăng Xuất Thành Công !!! ")';
-echo '</script>';
-echo '<script type="text/javascript">window.location = "../index.php" </script>';
